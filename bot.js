a6b1dc2b3e626daaf2b746eec0c5a0f3d2da40fa
AADevelops <amunahmad12@gmail.com>
2021-04-19 17:23:17 -0400

    Added command handler.

--- bot.js
+++ bot.js
@@ -1,16 +1,37 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const fs = require("fs");
 require("dotenv").config();
 
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Reading Directory and Filtering JS Files.
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// On Ready Event
 client.on("ready", () => {
     console.log(`Bot ${client.user.tag} is ready.`);
 });
 
+// On Message Event
 client.on("message", (message) => {
-    if (message.content === "testing") {
-        message.channel.send("pong");
-    } else if (message.content === `${process.env.PREFIX}shutdown`) {
-        client.destroy();
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+    try {
+        command.execute(message, args, client);
+    } catch (error) {
+        console.error(error);
+        message.reply("ERROR: Cannot execute that command.");
     }
 });
 
