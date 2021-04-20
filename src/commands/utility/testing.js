75a11d4c5c748bd014ad3d7fdf7700ca93223d3b
AADevelops <amunahmad12@gmail.com>
2021-04-20 16:16:05 -0400

    Added arguments to testing command.

--- src/commands/utility/testing.js
+++ src/commands/utility/testing.js
@@ -1,7 +1,13 @@
 module.exports = {
     name: "testing",
     description: "Testing command.",
-    execute(message) {
-        message.channel.send("Test Successful.");
+    execute(message, args) {
+        if (!args.length) {
+            message.channel.send("Test Successful.");
+        } else if (args[0] === "hi" || args [1] === "bye") {
+            message.channel.send("Test Argument(s) Worked!");
+        } else {
+            message.channel.send("Invalid Argument(s).");
+        }
     }
 }
