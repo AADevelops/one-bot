ba77c95fe777144aa57ccf483c77b764df36fe9b
AADevelops <amunahmad12@gmail.com>
2021-04-24 18:52:54 -0400

    Minor fixes.

--- src/commands/utility/testing.js
+++ src/commands/utility/testing.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args) {
         if (!args.length) {
             message.channel.send("Test Successful.");
-        } else if (args[0] === "hi" || args [1] === "bye") {
+        } else if (args[0] === "hi" || args[1] === "bye") {
             message.channel.send("Test Argument(s) Worked!");
         } else {
             message.channel.send("Invalid Argument(s).");
