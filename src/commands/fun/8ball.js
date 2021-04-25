c4aad3a285d5196cd2abf84f8bdc4dcf1eb0a60a
AADevelops <amunahmad12@gmail.com>
2021-04-25 15:48:08 -0400

    Added fun commands category with 8ball command.

---
+++ src/commands/fun/8ball.js
@@ -0,0 +1,33 @@
+module.exports = {
+    name: "8ball",
+    description: "Used for fortune-telling or seeking advice.",
+    usage: `${process.env.PREFIX}8ball <question>`,
+    execute(message, args) {
+        if (!args.length) {
+            return message.channel.send(`${message.author}, you must include the question after the command.`);
+        }
+
+        responses = ["It is certain.", 
+        "It is decidedly so.", 
+        "Without a doubt.", 
+        "Yes - definitely.", 
+        "You may rely on it.", 
+        "As I see it, yes.", 
+        "Most likely.", 
+        "Outlook good.", 
+        "Yes.", 
+        "Signs point to yes.", 
+        "Reply hazy, try again.", 
+        "Ask again later.", 
+        "Better not tell you now.",
+        "Can't predict now.", 
+        "Concentrate, and ask again.", 
+        "Don't count on it.", 
+        "My reply is no.", 
+        "My sources say no.", 
+        "Outlook not so good.", 
+        "Very doubtful."]
+
+        message.channel.send(`Question: ${args.join(" ")} ${"\n"}Response: ${responses[Math.floor(Math.random() * 20)]}`);
+    }
+}
