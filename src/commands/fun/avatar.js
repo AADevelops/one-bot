31e889bc075a0a86374a1fff61db5f42e0563b0f
AADevelops <amunahmad12@gmail.com>
2021-04-29 13:55:57 -0400

    Added avatar command.

---
+++ src/commands/fun/avatar.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "avatar",
+    aliases: ["icon", "pfp"],
+    description: "Displays the profile picture of a member.",
+    usage: ";avatar [optional<userMention>]",
+    execute(message, args) {
+        if (!args.length) {
+            message.channel.send(message.author.displayAvatarURL());
+        } else if (args.length) {
+            const argTag = args[0].match(/<[@#][!&]?[0-9]+>/g);
+            if (args[0] === "@everyone" || args[0] === "@here") {
+                return message.channel.send(`${message.author}, please do not ping everyone.`);
+            } else if (argTag) {
+                const member = message.mentions.members.first();
+                message.channel.send(member.user.displayAvatarURL());
+            } else {
+                message.channel.send(`${message.author}, the member is not in the server or you have not pinged correctly.`);
+            }
+        } 
+    }
+}
