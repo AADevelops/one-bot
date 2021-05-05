58af673b778ea250a0ad84c0691143a04edf2126
AADevelops <amunahmad12@gmail.com>
2021-05-05 19:17:44 -0400

    Added binary command.

---
+++ src/commands/fun/binary.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "binary",
+    description: "Used to convert a decimal number into binary.",
+    usage: `${process.env.PREFIX}binary <decimalNumber>`,
+    execute(message, args) {
+        if (!args.length) {
+            message.channel.send(`${message.author}, you must include the decimal number after the command.`)
+        } else {
+            const result = (parseInt(args[0])).toString(2);
+
+            if (result === "NaN") {
+                return message.channel.send(`${message.author}, wrong command usage. Please use \`;help binary\` for the command usage.`);
+            }
+
+            message.channel.send(`${message.author} ${"\n"}Decimal Number: ${args[0]} ${"\n"}Binary Number: ${result}`);
+        }
+    }
+}
