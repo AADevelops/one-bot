f218e2d3ac01e15d7ed896ac95f2f59f09830df5
AADevelops <amunahmad12@gmail.com>
2021-05-27 19:35:36 -0400

    Changed file structure.

--- src/commands/member/help.js
+++ src/commands/member/help.js
@@ -1,14 +1,15 @@
+// TODO: Make sections based on categories.
+
 module.exports = {
     name: "help",
     description: "Displays all commands.",
-    usage: `${process.env.PREFIX}help <commandName>`,
+    usage: `${process.env.PREFIX}help <category or commandName>`,
     execute(message, args) {
         const { commands } = message.client
         const information = [];
 
         if (!args.length) {
-            message.channel.send(`Commands: ${commands.map(command => command.name).join(", ")}`);
-            message.channel.send("If you would like to have more detail for a command, use `;help <commandName>`");
+            message.channel.send(`Commands: ${commands.map(command => command.name).join(", ")} ${"\n"}If you would like to have more detail for a command, use \`;help <commandName>\``);
         } else if (args.length) {
             const name = args[0].toLowerCase();
             const command = commands.get(name) || commands.find(cmd => cmd.aliases && cmd.aliases.includes(name));
@@ -18,7 +19,6 @@ module.exports = {
             }
 
             information.push(`**Command Name:** ${command.name}`);
-
             if (command.aliases) information.push(`**Command Aliases:** ${command.aliases.join(", ")}`);
             if (command.description) information.push(`**Description:** ${command.description}`);
             if (command.usage) information.push(`**Usage:** ${command.usage}`);
